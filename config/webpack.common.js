98cd0117b3447af2cacbe6ba56fb400e5471e4cc
cho <lll0858630@gmail.com>
2022-12-26 19:40:23 +0900

    웹팩 세팅중

---
+++ config/webpack.common.js
@@ -0,0 +1,3 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //script 태그를 사용하여 body에 모든 webpack 번들을 포함하는 HTML5 파일을 생성. 수동으로 컴파일해야하는 번거로움을 덜어주고 변경할 때마다 파일을 수정하지 않게 해준다. -> ex: dist/~.html 파일 생성
+// const dotenv = require('dotenv');
+// dotenv.config();
