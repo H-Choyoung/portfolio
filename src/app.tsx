a37c6fc599e93d3deb1d404b12377ced16bb44bb
cho <lll0858630@gmail.com>
2022-12-28 18:59:10 +0900

    관 이벤트 테스트 완료

--- src/app.tsx
+++ src/app.tsx
@@ -1,53 +1,82 @@
-import { useCallback, useRef } from 'react';
+import { useCallback, useRef, useState } from 'react';
+import styled,{ keyframes } from 'styled-components';
 import "./App.css";
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
 
 const App = () => {
+  const [clicked, setClicked] = useState<any>("");
+  const [display, setDisplay] = useState<any>("none");
+  const [skullKey, setSkullKey] = useState<any>("");
+
   let skullSpecial = "/img/skull_special.svg";
   let coffinCloseB = "/img/coffin_close_black.svg";
   let coffinOpenB = "/img/coffin_open_black.svg";
 
-  const openCoffin = useCallback(() => {
-    let coffin = document.getElementById("coffinCloseB");
-    coffin.style.transition = "0.8s";
-    coffin.style.rotate = "23deg";
-    setTimeout(()=> {
-      coffin.style.transition = "1s";
-      coffin.style.top = "1rem";
-      coffin.style.left = "4rem";
-    }, 1000);
+  const CoffinKey = keyframes`
+    0% {left: 0px;}
+    100% {left: 50px; rotate: 23deg;}
+  `
+  const CoffinStyle = styled.img`
+    position: absolute; 
+    z-index: 2; 
+    animation: ${clicked} 1.3s ease-out;
+    animation-fill-mode: forwards;
+    `
 
-    let skull = document.getElementById("skull");
-    setTimeout(() => {
-      skull.style.transition = "1s";
-      skull.style.display = "block";
-      skull.style.rotate = "-20deg";
-      skull.style.left = "2rem";
-    }, 1500);
-  }, []);
-  
+  const SkullKey = keyframes`
+    0% {right: 0px; opacity: 0;}
+    50% {right: 10px; opacity: 0%;}
+    70% {opacity: 0%;}
+    100% {right: 70px; rotate: -23deg; opacity: 100%;}
+  `
+  const SkullStyle = styled.img`
+    display: ${display};
+    position: absolute;
+    width: 100px;
+    top: 1rem;
+    left: 2.5rem;
+    animation: ${skullKey} 1.5s ease;
+    animation-fill-mode: forwards;
+    transition: 0.3s;
+    `
+
+  const onClicked =()=> {
+    if (clicked === "" && display === "none"){
+      setClicked(CoffinKey)
+      setDisplay("block")
+      setSkullKey(SkullKey);
+    } else {
+      setClicked("");
+      setDisplay("none");
+      setSkullKey("");
+    }
+  }
 
   return (
     <>
-    <div className="clicked">
-      <img id="coffinCloseB" 
-      src={coffinCloseB} 
-      style={{
-        position:"absolute", 
-        zIndex:"1"}}
-      onClick={openCoffin}
-      >
-      </img>
+    <div>
+      <CoffinStyle
+        id='coffin'
+        src={coffinCloseB}
+        onClick={onClicked}
+        ></CoffinStyle>
     </div>
-      <img id="skull"
+      <SkullStyle
         src={skullSpecial}
-        style={{
-          display: "none",
-          position: "absolute", 
-          width: "100px",
-          top: "1rem",
-          left: "2.5rem" }}
-      ></img>
+      ></SkullStyle>
       <img src={coffinOpenB}></img>
+      <div>
+        {/* <Player
+          autoplay
+          loop
+          src="/img/129232-rewards-with-confetti.json"
+          style={{
+            width:'300px',
+            height:'300px'
+          }}
+        >
+        </Player> */}
+      </div>
     </>
   )
 }
